520996b2efb854ada79afc75b1d8f160a7f95ed5
alex4150 <76718561+alex4150@users.noreply.github.com>
2021-11-29 11:17:13 +1100

    CT
    CT

---
+++ ctfetchPost1.js
@@ -0,0 +1,15 @@
+const todo = {
+    activity_id: 410088344
+};
+
+fetch('https://api217.crowdtwist.com/v2/users/alexrewards99@gmail.com/activities?api_key=2FJQiWvADwF1NHME8V7B3UmzhzyfGGUn&id_type=email', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+    });
